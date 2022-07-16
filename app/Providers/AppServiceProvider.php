6acaa8cdc494d648ef170a4102072dd0ce9d1ade
Hlaing Win Phyoe <hlaingwinphyoedev@gmail.com>
2022-07-16 12:04:03 +0630

    provider-change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
 
         Blade::if('admin',function (){
